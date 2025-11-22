4e423934b8d1284d72a1166ddb9b83e57f7bc68a
Spacedwog <owner@spacedwog.com.br>
2025-11-22 15:15:15 -0300

    Commited

--- spacecworp/App.tsx
+++ spacecworp/App.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import connectTcpServer from './TcpClient';
 
 export default function App() {
+  function handleConnect() {
+    connectTcpServer('192.168.0.100', 8000); // Altere para IP/porta do servidor desejado
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Connect TCP/IP" onPress={handleConnect} />
       <StatusBar style="auto" />
     </View>
   );
