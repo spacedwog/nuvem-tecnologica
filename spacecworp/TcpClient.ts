a2ebac08561e1adcc41c92b1cfe60ab9aec9cd08
Spacedwog <owner@spacedwog.com.br>
2025-11-22 17:45:00 -0300

    Commited

--- spacecworp/TcpClient.ts
+++ spacecworp/TcpClient.ts
@@ -4,7 +4,16 @@ export function connectWebSocketServer({ url, onLog }: { url: string; onLog: (ms
   ws.onopen = () => onLog("WebSocket conectado!");
   ws.onmessage = ({ data }) => onLog("Recebido: " + data);
   ws.onerror = (err) => onLog("Erro WebSocket: " + JSON.stringify(err));
-  ws.onclose = () => onLog("WebSocket fechado."); // Evento de fechamento aqui!
+  ws.onclose = () => onLog("WebSocket fechado.");
 
   return ws;
+}
+
+export function sendWebSocketData(ws: WebSocket, message: string, onLog?: (msg: string) => void) {
+  if (ws && ws.readyState === WebSocket.OPEN) {
+    ws.send(message);
+    if (onLog) onLog("Enviado: " + message);
+  } else {
+    if (onLog) onLog("WebSocket não está conectado.");
+  }
 }
