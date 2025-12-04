0f76e5c151147ad9d0f7121763bb1842e3fb67c2
Spacedwog <owner@spacedwog.com.br>
2025-12-04 14:19:50 -0300

    Commited

---
+++ env.d.ts
@@ -0,0 +1,12 @@
+declare module '@env' {
+  export const AUTH_USERNAME: string;
+  export const MY_PAT: string;
+  export const AUTH_PASSWORD: string;
+  export const GITHUB_TOKEN: string;
+  export const GITHUB_OWNER: string;
+  export const GITHUB_REPO: string;
+  export const VERCEL_URL: string;
+  export const ESP32_SOFTAP_IP: string;
+  export const ESP32_STA_IP: string;
+  export const VERCEL_TOKEN: string;
+}
